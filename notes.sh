18477372ee29e698320b4312b3545ac8e7d133ef
Erik <92889691+erikrl2@users.noreply.github.com>
2022-10-30 17:30:11 +0100

    Fix case with specified module

--- notes.sh
+++ notes.sh
@@ -69,22 +69,24 @@ get_notes() {
 make_notes() {
 	for semester in "$NOTES_DIR"/sem$1; do
 		if ! [[ $(basename "$semester") =~ ^sem[0-9]+$ ]]; then continue; fi
-		for module in "$semester"/$2; do
+		if [[ "$2" == "*" ]]; then modules=$(for f in "$semester"/*; do echo $f; done)
+		else modules="$semester/$2"; fi
+		oldifs=$IFS
+		IFS=$'\n'
+		for module in $modules; do
 			if ! [[ -d "$module" ]]; then continue; fi
 			cd "$module"
 			notes=$(get_notes)
 			output_file="$module/"$(basename "$module")".html"
-			oldifs=$IFS
-			IFS=$'\n'
 			pandoc --template="../../theme/template.html" \
 				-c "../../theme/css/theme.css" -c "../../theme/css/skylighting-paper-theme.css" \
 				--katex --toc --toc-depth=1 -M title=$(basename "$module") \
 				-s -o "$output_file" "$NOTES_DIR/metadata.md" $notes &&
 				echo "Created $output_file"
-			IFS=$oldifs
 			unset notes output_file oldifs
 			cd - &>/dev/null
 		done
+		IFS=$oldifs
 	done
 }
 
@@ -99,7 +101,7 @@ main() {
 				echo "Compiling semster ${1}..."
 				make_notes "$1" "*"
 			elif [[ -d "$NOTES_DIR/sem$1/$2" ]]; then
-				echo "Compiling semester $1 / module ${2}..."
+				echo "Compiling semester $1 / ${2}..."
 				make_notes "$1" "$2"
 			else
 				echo "Module $2 does not exist!"; exit 1
